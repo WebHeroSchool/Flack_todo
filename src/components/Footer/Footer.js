f5b618e7cd2b4ee8ad907700f60649e55d5ebbd5
Natasha <nataly815@gmail.com>
2020-12-11 00:08:29 +0300

    created some styles for app

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import styles from './Footer.module.css';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 
 const Footer = ({count}) => (
 <div>
-  Еще осталось сделать: {count} дел(а)
+  <p className={styles.text}>Еще осталось сделать: {count} дел(а)</p>
+<ButtonGroup className={styles.buttons} variant="text" color="default" aria-label="text primary button group">
+  <Button>Все</Button>
+  <Button>Активные</Button>
+  <Button>Выполненные</Button>
+  <Button>Удалить выполненные дела</Button>
+</ButtonGroup>
 </div>
 );
 
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import Item from '../Item/Item';
+import styles from './ItemList.module.css';
+import Grid from '@material-ui/core/Grid';
 
 const ItemList = ({ items }) => (
-  <ul>
+  <ul className={styles.list}>
     {items.map(item => <li key={item.value}>
-      <Item value={item.value} isDone={item.isDone} /></li>)}
+      <Grid
+    container
+    direction="column"
+    justify="center"
+    alignItems="flex-start"
+    >
+    <Item value={item.value} isDone={item.isDone} /></Grid></li>)}
+    
   </ul>
 );
 
