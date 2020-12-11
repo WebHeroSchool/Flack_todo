61a98ac15ac798915dc5e25ac223e9e05f73edc5
Natasha <nataly815@gmail.com>
2020-12-11 11:50:12 +0300

    component-class replace component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,30 +4,32 @@ import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
-const App = () => {
-  const items = [
-    {
-      value: 'Написать новое приложение',
-      isDone: true
-    },
-    {
-      value: 'прописать props',
-      isDone: false
-    },
-    {
-      value: 'изучить тему',
-      isDone: false
-    }
-  ];
-
-  return (
-  <div className={styles.wrap}>
-    <h1 className={styles.title}>todos</h1>
-    <InputItem />
-    <ItemList items={items} />
-    <Footer count={2} />
-  </div>
-  );
-}
+class App extends React.Component {
+  render() {
+    const items = [
+      {
+        value: 'Написать новое приложение',
+        isDone: true
+      },
+      {
+        value: 'прописать props',
+        isDone: false
+      },
+      {
+        value: 'изучить тему',
+        isDone: false
+      }
+    ];
+  
+    return (
+    <div className={styles.wrap}>
+      <h1 className={styles.title}>todos</h1>
+      <InputItem />
+      <ItemList items={items} />
+      <Footer count={2} />
+    </div>
+    );
+  }
+};
 
 export default App;
