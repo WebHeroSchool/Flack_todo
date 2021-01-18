24aa5b425018af580839029dcefe8bc638d8b363
Natasha <nataly815@gmail.com>
2021-01-18 17:04:41 +0300

    adaptive is added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,20 +5,24 @@ import MenuList from '@material-ui/core/MenuList';
 import styles from './App.module.css';
 import Todo from '../Todo/Todo';
 import About from '../About/About';
+import WebIcon from './img/WHS.png'
 
 const App = () => (
-  <Router className={styles.menu}>
-    <div className={styles.wrap}>
-          <MenuList>
-            <Link to='/' className={styles.link}><MenuItem>Обо мне</MenuItem></Link>
-            <Link to='/todo' className={styles.link}><MenuItem>Задачи</MenuItem></Link>
-          </MenuList>
-          <div className={styles.content}>
-            <Route path='/' exact component={About} />
-            <Route path='/todo' component={Todo} />
-          </div>
-    </div>
-  </Router>
+  <div>
+  <div><img className={styles.school} src={WebIcon}></img></div>
+    <Router>
+      <div className={styles.wrap}>
+            <MenuList className={styles.menulist}>
+              <Link to='/' className={styles.link}><MenuItem>Обо мне</MenuItem></Link>
+              <Link to='/todo' className={styles.link}><MenuItem>Задачи</MenuItem></Link>
+            </MenuList>
+            <div className={styles.content}>
+              <Route path='/' exact component={About} />
+              <Route path='/todo' component={Todo} />
+            </div>
+      </div>
+    </Router>
+  </div>
 );
 
 export default App;
